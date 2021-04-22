a6bd772624dc7ad5e20a53e494df7ea9200676a1
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-04-22 10:29:47 +0200

    Add Laravel project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog, Text, simpledialog
 import os
+import getpass
 
 # Set window
 root = tk.Tk()
@@ -21,7 +22,8 @@ homepath = os.path.expanduser(os.getenv('USERPROFILE'))
 def generateProjectAngular():
 
     # Set repository destination
-    fileName = filedialog.askdirectory(title="Emplacement du projet")
+    fileName = filedialog.askdirectory(
+        title="Emplacement du projet", initialdir=homepath + "/Documents/Dev/Web/Front/Angular")
 
     # Set project name
     projectName = simpledialog.askstring(title="Nom",
@@ -40,11 +42,31 @@ def generateProjectAngular():
     # os.system("cmd /c ng serve --o")
 
 
+def generateProjectLaravel():
+
+    # Set repository destination
+    fileName = filedialog.askdirectory(
+        title="Emplacement du projet",  initialdir="C:/laragon/www")
+
+    # Set project name
+    projectName = simpledialog.askstring(title="Nom",
+                                         prompt="Quel est le nom de votre programme ? ")
+
+    # Generate project
+    os.chdir(fileName)
+    os.system("cmd /c composer create-project laravel/laravel " +
+              projectName + " --prefer-dist")
+
+    # Open in VS Code
+    os.chdir(fileName + "/" + projectName)
+    os.system("cmd /c code .")
+
+
 btnAngular = tk.Button(frame, text="Angular", padx=10,
                        pady=5, fg="white", bg="#c3002f", font=("Courrier, 14"), command=generateProjectAngular).pack(fill=X, expand=YES)
 
 btnLaravel = tk.Button(frame, text="Laravel", padx=10,
-                       pady=5, fg="white", bg="#ff2d20", font=("Courrier, 14"), command=generateProjectAngular).pack(fill=X, expand=YES)
+                       pady=5, fg="white", bg="#ff2d20", font=("Courrier, 14"), command=generateProjectLaravel).pack(fill=X, expand=YES)
 
 btnReactNative = tk.Button(frame, text="React Native", padx=10,
                            pady=5, fg="white", bg="#61dafb", font=("Courrier, 14"), command=generateProjectAngular).pack(fill=X, expand=YES)
