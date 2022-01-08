d1c1397f9452152bf76f531afaa4b86eccdb625b
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-09 00:48:10 +0100

    Remove open with code Add project React

--- main.py
+++ main.py
@@ -34,17 +34,30 @@ def generateProject(project):
 
         if fileName.strip():
             # Set project name
-            projectName = simpledialog.askstring(title="Nom",
-                                                 prompt="Quel est le nom de votre programme ? ")
+            projectName = simpledialog.askstring(title="Name",
+                                                 prompt="What's the name of your project ? ")
 
             # Generate project
             os.chdir(fileName)
             os.system("cmd /c ng new " + projectName +
                       " --style=scss --routing --strict")
 
-            # Open in VS Code
-            os.chdir(fileName + "/" + projectName)
-            os.system("cmd /c code .")
+  # React
+      if project == "react":
+
+          # Set repository destination
+          fileName = filedialog.askdirectory(
+              title="Emplacement du projet", initialdir=homepath + "/Documents/Dev/Web/Front/React")
+
+          if fileName.strip():
+              # Set project name
+              projectName = simpledialog.askstring(title="Name",
+                                                   prompt="What's the name of your project ? ")
+
+              # Generate project
+              os.chdir(fileName)
+              os.system("cmd /c npx create-react-app " + projectName)
+
 
     # Laravel
     elif project == "laravel":
@@ -55,17 +68,14 @@ def generateProject(project):
 
         if fileName.strip():
             # Set project name
-            projectName = simpledialog.askstring(title="Nom",
-                                                 prompt="Quel est le nom de votre programme ? ")
+            projectName = simpledialog.askstring(title="Name",
+                                                 prompt="What's the name of your project ? ")
 
             # Generate project
             os.chdir(fileName)
             os.system("cmd /c composer create-project laravel/laravel " +
                       projectName + " --prefer-dist")
 
-            # Open in VS Code
-            os.chdir(fileName + "/" + projectName)
-            os.system("cmd /c code .")
 
     # React Native
     elif project == "react_native":
@@ -76,17 +86,14 @@ def generateProject(project):
 
         if fileName.strip():
             # Set project name
-            projectName = simpledialog.askstring(title="Nom",
-                                                 prompt="Quel est le nom de votre programme ? ")
+            projectName = simpledialog.askstring(title="Name",
+                                                 prompt="What's the name of your project ? ")
 
             # Generate project
             os.chdir(fileName)
             # os.system("cmd /c composer create-project laravel/laravel " +
             #           projectName + " --prefer-dist")
 
-            # Open in VS Code
-            os.chdir(fileName + "/" + projectName)
-            os.system("cmd /c code .")
 
     # Ionic
     elif project == "ionic":
@@ -97,21 +104,21 @@ def generateProject(project):
 
         if fileName.strip():
             # Set project name
-            projectName = simpledialog.askstring(title="Nom",
-                                                 prompt="Quel est le nom de votre programme ? ")
+            projectName = simpledialog.askstring(title="Name",
+                                                 prompt="What's the name of your project ? ")
 
             # Generate project
             os.chdir(fileName)
             os.system("cmd /c ionic start " +
                       projectName + " blank --type=angular")
 
-            # Open in VS Code
-            os.chdir(fileName + "/" + projectName)
-            os.system("cmd /c code .")
 
 
 tk.Button(frame, text="Angular", padx=10,
           pady=5, fg="white", bg="#c3002f", font=("Courrier, 14"), command=lambda: generateProject("angular")).pack(fill=X, expand=YES)
+
+tk.Button(frame, text="React", padx=10,
+        pady=5, fg="white", bg="#61DAFB", font=("Courrier, 14"), command=lambda: generateProject("react")).pack(fill=X, expand=YES)
 
 tk.Button(frame, text="Laravel", padx=10,
           pady=5, fg="white", bg="#ff2d20", font=("Courrier, 14"), command=lambda: generateProject("laravel")).pack(fill=X, expand=YES)
