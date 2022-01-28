4e97e29f38f744232b64839ca27b0dc9a34685aa
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-28 09:44:53 +0100

    Create variable for font

--- main.py
+++ main.py
@@ -19,22 +19,25 @@ frame = tk.Frame(root, bg="#4065A4").place(relwidth=1, relheight=1)
 # Title
 title = Label(root, text="Program Generator", font=("Courrier, 25"), fg="red", bg="#4065A4").pack()
 
+# Font
+fontBtn = "Courrier, 14"
 
-# Create interface
+
+# Generate buttons
 tk.Button(frame, text="Angular", padx=10,
-          pady=5, fg="white", bg="#c3002f", font=("Courrier, 14"), command=lambda: generate("angular")).pack(fill=X, expand=YES)
+          pady=5, fg="white", bg="#c3002f", font=(fontBtn), command=lambda: generate("angular")).pack(fill=X, expand=YES)
 
 tk.Button(frame, text="React", padx=10,
-        pady=5, fg="white", bg="#61DAFB", font=("Courrier, 14"), command=lambda: generate("react")).pack(fill=X, expand=YES)
+        pady=5, fg="white", bg="#61DAFB", font=(fontBtn), command=lambda: generate("react")).pack(fill=X, expand=YES)
 
 tk.Button(frame, text="NodeJS", padx=10,
-        pady=5, fg="white", bg="#79B461", font=("Courrier, 14"), command=lambda: generate("node")).pack(fill=X, expand=YES)
+        pady=5, fg="white", bg="#79B461", font=(fontBtn), command=lambda: generate("node")).pack(fill=X, expand=YES)
 
 tk.Button(frame, text="Laravel", padx=10,
-          pady=5, fg="white", bg="#ff2d20", font=("Courrier, 14"), command=lambda: generate("laravel")).pack(fill=X, expand=YES)
+          pady=5, fg="white", bg="#ff2d20", font=(fontBtn), command=lambda: generate("laravel")).pack(fill=X, expand=YES)
 
 tk.Button(frame, text="Ionic", padx=10,
-          pady=5, fg="white", bg="#4e8df7", font=("Courrier, 14"), command=lambda: generate("ionic")).pack(fill=X, expand=YES)
+          pady=5, fg="white", bg="#4e8df7", font=(fontBtn), command=lambda: generate("ionic")).pack(fill=X, expand=YES)
 
 
 root.mainloop()
